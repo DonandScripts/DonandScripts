fe36df87636302fc1f7c93d78a8e199b72fe4496
DonandScripts <90103097+DonandScripts@users.noreply.github.com>
2023-10-14 15:32:13 +0700

    Update Y9RmIXmHAfwi7XxqPwSAaRdL035FwA8QmZa32H9ajS4V6Isb4e.lua

--- Y9RmIXmHAfwi7XxqPwSAaRdL035FwA8QmZa32H9ajS4V6Isb4e.lua
+++ Y9RmIXmHAfwi7XxqPwSAaRdL035FwA8QmZa32H9ajS4V6Isb4e.lua
@@ -1,33 +1,33 @@
-    local LocalFlyingEntityModule = require(game.ReplicatedStorage.LocalFlyingEntity)
-    local ListCross = {}
-    local Cross
-    local plr = game.Players.LocalPlayer
-    local ParticleToolsModule = require(game.ReplicatedStorage.LocalFX.LocalTargetPracticeBeam)
-    game.Workspace.Particles.ChildAdded:Connect(function(v)
-        if v.Name == "Crosshair" and v ~= nil and v.BrickColor ~= BrickColor.new("Forest green") and v.BrickColor ~= BrickColor.new("Flint") then
-            Cross = v
-        end
-    end)
-    local old
-    old = hookfunction(ParticleToolsModule.Make,function(...) 
-        local rac = ...
-        if tostring(rac.Player)==plr.Name then 
-            if rac.Action == "Make" then 
-                Cross = nil
-                local a = old(...)
-                if Cross and rac and rac.FEID then 
-                    local att = LocalFlyingEntityModule.GetEntity(rac.FEID).Part
-                    if att then 
-                        if not ListCross[att] then 
-                            ListCross[att] = {}
-                        end
-                        table.insert(ListCross[att],Cross)
+local CrossHairList = {}
+local Cross
+local plr = game.Players.LocalPlayer
+local ParticleToolsModule = require(game.ReplicatedStorage.LocalFX.LocalTargetPracticeBeam)
+local LocalFlyingEntityModule = require(game.ReplicatedStorage.LocalFlyingEntity)
+game.Workspace.Particles.ChildAdded:Connect(function(v)
+    if v.Name == "Crosshair" and v ~= nil and v.BrickColor ~= BrickColor.new("Forest green") and v.BrickColor ~= BrickColor.new("Flint") then
+        Cross = v
+    end
+end)
+local old
+old = hookfunction(ParticleToolsModule.Make,function(...) 
+    local Arg = ...
+    if tostring(Arg.Player)==plr.Name then 
+        if Arg.Action == "Make" then 
+            Cross = nil
+            local a = old(...)
+            if Cross and Arg.FEID then 
+                local partcross = LocalFlyingEntityModule.GetEntity(Arg.FEID).Part
+                if partcross then 
+                    if not CrossHairList[partcross] then 
+                        CrossHairList[partcross] = {}
                     end
-                    
+                    table.insert(CrossHairList[partcross],Cross)
                 end
-                return a
+                
             end
+            return a
         end
-        return old(...)
-    end)
-    return ListCross
+    end
+    return old(...)
+end)
+return CrossHairList
